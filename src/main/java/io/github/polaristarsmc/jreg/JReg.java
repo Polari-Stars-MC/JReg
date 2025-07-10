a2268a39e489b5a9b7a6c1c3cb3015d1636a2d33
baka4n <qq474899581@gmail.com>
2025-07-10 18:42:59 +0800

    批量注册论

--- src/main/java/io/github/polaristarsmc/jreg/JReg.java
+++ src/main/java/io/github/polaristarsmc/jreg/JReg.java
@@ -1,19 +1,16 @@
 package io.github.polaristarsmc.jreg;
 
-import net.minecraft.world.item.BlockItem;
+import io.github.polaristarsmc.jreg.registry.JRegistries;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredItem;
-import net.neoforged.neoforge.registries.DeferredRegister;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.*;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 // The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(JReg.MODID)
@@ -21,24 +18,14 @@ public class JReg {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "jreg";
     public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
-    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
-    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-    public static final DeferredBlock<Block> TEST1BLOCK = BLOCKS.registerBlock("test1block", Block::new);
-    public static final DeferredItem<BlockItem> TEST1BLOCK$ITEM = ITEMS.registerSimpleBlockItem(TEST1BLOCK);
-    public static final ExecutorService threadFactory = Executors.newVirtualThreadPerTaskExecutor();
-    public static final CompletableFuture<DeferredItem<Item>>[] TEST = items(Item::new, "test", "test1", "test2", "test3", "test4", "test5", "test6");
-    public static final RegistryBatchRegister registryBatchRegister = new RegistryBatchRegister(BLOCKS,ITEMS);
+
+    public static final JRegistries REGISTRIES = new JRegistries(MODID);
+    public static final List<DeferredItem<Item>> TEST =
+            REGISTRIES.items(Item::new, "test", "test2", "test5", "test6", "test7", "test8", "test9", "test10");
     public JReg(IEventBus bus) {
-        CompletableFuture.allOf(TEST);
-        registryBatchRegister.registerAll(bus);
+        REGISTRIES.registerAll(bus);
+
     }
 
-    public static <T extends Item> CompletableFuture<DeferredItem<T>>[] items(Function<Item.Properties, T> function, String... names) {
-        CompletableFuture<DeferredItem<T>>[] future = new CompletableFuture[names.length];
-        for (int i = 0; i < names.length; i++) {
-            int finalI = i;
-            future[i] = CompletableFuture.supplyAsync(() -> ITEMS.registerItem(names[finalI], function), threadFactory);
-        }
-        return future;
-    }
+
 }
--- src/main/java/io/github/polaristarsmc/jreg/registry/JRegistries.java
+++ src/main/java/io/github/polaristarsmc/jreg/registry/JRegistries.java
@@ -1,28 +1,73 @@
 package io.github.polaristarsmc.jreg.registry;
 
+import com.google.common.base.Function;
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Supplier;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Stream;
 
 /**
  * @author baka4n
  * @code @Date 2025/7/10 13:31:51
  */
-public class JRegistries {
-    public static final Map<ResourceKey<? extends Registry<?>>, JRegistry<?, ?>> REGISTRY = new HashMap<>();
+public class JRegistries implements AutoCloseable {
+    // multi-thread support
+    public final ConcurrentHashMap<ResourceKey<? extends Registry<?>>, DeferredRegister<?>> registries = new ConcurrentHashMap<>();
     public final String modid;
+    private final ExecutorService executor;
+
+    public DeferredRegister.Items items() {
+        if (registries.containsKey(Registries.ITEM)) {
+            return (DeferredRegister.Items) registries.get(Registries.ITEM);
+        }
+        DeferredRegister.Items items = DeferredRegister.createItems(modid);
+        registries.put(Registries.ITEM, items);
+        return items;
+    }
+
+    public <T extends Item> DeferredItem<T> item(String name, Function<Item.Properties, T> function) {
+        return items().registerItem(name, function);
+    }
+
+    public <T extends Item> List<DeferredItem<T>> items(Function<Item.Properties, T> function, String... names) {
+        CompletableFuture<DeferredItem<T>>[] futures = new CompletableFuture[names.length];
+
+        DeferredRegister.Items items = items();
+        for (int i = 0; i < names.length; i++) {
+            int finalI = i;
+            futures[i] = CompletableFuture.supplyAsync(() -> items.registerItem(names[finalI], function), executor);
+        }
+        CompletableFuture.allOf(futures);
+        return Arrays.stream(futures).map(CompletableFuture::join).toList();
+    }
+
 
     public JRegistries(String modid) {
         this.modid = modid;
+        executor = Executors.newVirtualThreadPerTaskExecutor();
     }
 
-    @SuppressWarnings("unchecked")
-    public <T> JRegistry<T, JRegistries> register(ResourceKey<? extends Registry<T>> key, Supplier<T> value) {
-        JRegistry<T, JRegistries> registry = (JRegistry<T, JRegistries>) REGISTRY.getOrDefault(key, new JRegistry<>(DeferredRegister.create(key, modid), this));
-        return registry;
+    public void registerAll(IEventBus modEventBus) {
+        for (DeferredRegister<?> register : registries.values()) {
+            executor.submit(() -> register.register(modEventBus));
+        }
+        close();
+    }
+
+    @Override
+    public void close() {
+        executor.shutdown();
     }
 }
